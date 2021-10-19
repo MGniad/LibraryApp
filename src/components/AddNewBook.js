d4c9f56d7918bd2ca45169b294d3d80ed3eceb8d
Marek <marek.j.gniady@gmail.com>
2021-10-19 15:33:26 +0200

    add modal component

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Modal from './Modal'
 
 function AddNewBook(){
-    
+    const [showModal, setShowModal] = useState(false)
     return (
         <div className='AddNewBook'>
-            AddNewBook
+            <div className="btn">
+            <button onClick={ () => setShowModal(true)}>
+                + New Book
+            </button>
+            </div>
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <div>
+                    hello world
+                </div>
+                <button 
+                onClick={ () => setShowModal(false)}>
+                    hide
+                </button>
+
+            </Modal>
         </div>
     )
 }
